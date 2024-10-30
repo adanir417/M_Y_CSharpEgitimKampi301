192698d2fdebf66935cfca5f9031e200a062a3a6
adanir417 <adanir417@hotmail.com>
2024-10-30 20:44:54 +0300

    Lecture eleventh completed.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+
+
+
+        //field - Variable - Property
+
+        // Bir değişken sınıfın içine tanımlanırsa field
+        // Eğer değişken yapısı get ve set olarak değer alırsa property
+        // Eğer method içerisinde tanımlanırsa değişken.
+
+        //Otomatik artan bir id olduğunu tanımlamak için SınıfAdı + Id eki ile tanımlama yapmalı
+        // Category sınıfı için public int CategoryId { get; set; }
+
+        //Tüm bu sınıf ile ilgili olan property'ler tablolarda sütunlara(Column) karşılık gelecek.
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # CSharpEgitimKampi301
+
+---
+
+Murat YÜCEDAĞ [C# Eğitim Kampı](https://www.youtube.com/playlist?list=PLKnjBHu2xXNPmFMvGKVHA_ijjrgUyNIXr)    
+Check original repository => [C# Eğitim Kampı Original Repository](https://github.com/MuratYucedag/CSharpEgitimKampi)
+
+---
+
+
+
+## Dersler
+### Module 301 - Continue    
+---    
+### :green_circle: Ders 11 - OOP Modülü: C# ile N Katmanlı Mimari Entity Layer    
+Boş bir solution içinde katmanlar nasıl eklenir?, Entity içinde tablolara karşılık gelecek sınıflar nasıl oluşturulur?,    
+Oluşturulan sınıflar içindeki Property yapıları tablolarda hangi konuma işaret eder? Ve Erişim belirleyiciler nasıl işler?    
+Bu ders içerisinde bunları anladım.
